babddde7a0ce2962ab2032e14579f8040fa8d317
Muaz Ahmad <muazahmad787@gmail.com>
2025-05-26 04:34:52 +0500

    Errors Corrected

--- fyp/lib/services/api_service.dart
+++ fyp/lib/services/api_service.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
-
+ 
 class ApiService {
   static const String _baseUrl = 'http://192.168.18.63:5000/api/predict';
 
   static Future<String> uploadImage(File image) async {
     var request = http.MultipartRequest('POST', Uri.parse('$_baseUrl/predict'));
     
-    request.files.add(await http.MultipartFile.fromPath(
+    request.files.add(await http.MultipartFile. fromPath(
       'image',
       image.path,
       contentType: MediaType('image', 'jpeg'),
