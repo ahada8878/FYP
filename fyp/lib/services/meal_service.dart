ece6e2ae5e8de0a4d3b10e43dea09c2ecd7d825e
Hanzalah-Ali-Taj <hanzalahali39@gmail.com>
2025-10-09 22:13:44 +0500

    Han3

--- fyp/lib/services/meal_service.dart
+++ fyp/lib/services/meal_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import '../app_config.dart';
+import 'config_service.dart';
 
 class MealService {
   static const String baseUrl = 'http://$apiIpAddress:5000/api/mealplan';
